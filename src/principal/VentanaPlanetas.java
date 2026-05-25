810206c64c8333ba398638ca33cc4819bd19e180
dibaos2007-ship-it <dibaos2007@gmail.com>
2026-05-25 02:11:11 -0500

    ya le meti los botones pero aun no hacen nada

--- src/principal/VentanaPlanetas.java
+++ src/principal/VentanaPlanetas.java
@@ -1,4 +1,21 @@
 package principal;
 
+import javax.swing.*;
+
 public class VentanaPlanetas {
+    private JPanel panel1;
+    private JPanel PanelIzquierdo;
+    private JTextField txtidTextField;
+    private JTextField txtNombreTextField;
+    private JTextField txtGalaxiaTextField;
+    private JTextField txtTipoTextField;
+    private JPanel PanelDerecho;
+    private JButton btnBuscarPorid;
+    private JButton btnVerTodos;
+    private JButton btnAgregar;
+    private JButton btnFiltrarGalaxia;
+    private JButton BtnFiltrarVida;
+    private JPanel PanelInferior;
+    private JTextArea textArea1;
 }
+
